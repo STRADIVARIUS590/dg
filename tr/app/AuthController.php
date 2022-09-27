2fb096085757ce16a78df26e4b593aa0f832795e
Cristian  Joel <56978505+STRADIVARIUS590@users.noreply.github.com>
2022-09-27 10:14:57 -0600

    Add files via upload

---
+++ tr/app/AuthController.php
@@ -0,0 +1,67 @@
+<?php
+
+var_dump($_POST);
+
+ if(isset($_POST['action'])){
+    switch($_POST['action']){
+        case 'access':  
+            $authC = new AuthCotroller;
+            $email = $_POST['name'];
+            $password = $_POST['pass'];
+
+            $c = new AuthCotroller();
+             $c->login($email, $password);
+
+    }
+}
+
+
+
+/* $c = new AuthCotroller();
+ $c->login('crve_19@alu.uabcs.mx',  'A9*27rh6%#271N');
+ */
+ 
+class AuthCotroller{
+
+
+    public function login($email, $password){
+        
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'https://crud.jonathansoto.mx/api/login',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'POST',
+  CURLOPT_POSTFIELDS => array('email' =>  $email,'password' => $password),
+
+));
+
+$response = curl_exec($curl);
+$response = json_decode($response);
+curl_close($curl);
+
+if(isset($response->code) && $response->code >0){
+    session_start();
+    $_SESSION['name'] = $response->data->name;
+    $_SESSION['lastname'] = $response->data->lastname;
+    $_SESSION['avatar'] = $response->data->avatar;
+    $_SESSION['token'] = $response->data->token;
+
+    header('Location:../products');
+    
+}else{
+    
+    header('Location:..index.php');
+}
+
+echo $response;
+
+    }
+}
+
+?>
